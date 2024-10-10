89fd29eb142dd2291164b0930d69e855552dbadf
UlfaAnisa <ulfaanisa@gmail.com>
2024-10-10 17:12:20 +0700

    tugas 6

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,14 +1,14 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tugas 5</title>
+    <title>WLTugas2</title>
     <style>
         body {
-            font-family: Arial, sans-serif;
+            font-family: Monrovia, monrovia;
+            font-weight: bold;
             display: flex;
             justify-content: center;
             align-items: center;
@@ -16,42 +16,55 @@
             margin: 0;
             background-color: #800080;
         }
-
         .profile-container {
             text-align: center;
-            background-color: white;
-            padding: 20px;
-            border-radius: 10px;
+            background-color: #CA226B;
+            padding: 100px;
+            border-radius: 50px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            width: 300px;
         }
-
         .profile-container img {
-            width: 100px;
-            height: 100px;
-            border-radius: 50%;
-            margin-bottom: 20px;
+            width: 150px;
+            height: 150px;
+            border-radius: 100%;
+            margin-bottom: 50px;
         }
-
         .profile-container .info {
-            background-color: #800080;
+            background-color: #ddd;
             margin: 10px 0;
             padding: 10px;
             border-radius: 5px;
+        }
+        .profile-info {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        .info-item {
+            background-color: #d9dddc;
+            color: #222021;
+            margin: 10px 0;
+            padding: 10px;
+            width: 100%;
+            text-align: center;
+            border-radius: 10px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            font-size: 16px;
         }
     </style>
 </head>
 
 <body>
 <div class="profile-container">
-    <img src="https://tse3.mm.bing.net/th?id=OIP.7v1NRcDNndfz2GtLSef8cgHaHa&pid=Api&P=0&h=220" alt="Minions">
-    <div class="info">
-        <strong>Nama:</strong> {{ $nama }}
-    </div>
-    <div class="info">
-        <strong>Kelas:</strong> {{ $nama_kelas ?? 'Kelas tidak ditemukan' }}
-    </div>
-    <div class="info">
-        <strong>NPM:</strong> {{ $npm }}
+        <div class="profile-picture">
+            <img src="{{ asset('img/' . $user->foto) }}" alt="Profile Picture" />
+        </div>
+
+    <div class="profile-info">
+        <div class="info-item">Nama : {{ $user->nama }}</div>
+        <div class="info-item">NPM : {{ $user->npm }}</div>
+        <div class="info-item">Kelas : {{ $user->nama_kelas ?? 'Kelas tidak ditemukan' }}</div>
     </div>
 </div>
 
